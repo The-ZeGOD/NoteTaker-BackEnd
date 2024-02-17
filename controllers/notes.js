26a88d9a303f5e9973a1414856183d1085bb11f9
The-ZeGOD <nihalgupta.nirmal@gmail.com>
2024-02-17 15:53:08 +0530

    note routes completed

--- controllers/notes.js
+++ controllers/notes.js
@@ -15,5 +15,74 @@ exports.addNote = (req, res) => {
         });
     }
 
-    client.query(`INSERT INTO notes (email, heading, content) VALUES ('${req.email}')`)
+    client
+        .query(`INSERT INTO notes (email, heading, content) VALUES ('${req.email}', '${heading}', '${content}');`)   
+        .then((data)=>{
+            res.status(200).json({
+                message: "Note Added Successfully",
+            });
+        })
+        .catch((err)=>{
+            res.status(400).json({
+                error: "Database Error Occured"
+            });
+        });
+}; 
+
+exports.getAllNotes = (req, res) => {
+    client
+        .query(`SELECT * FROM notes WHERE email = '${req.email}'`)
+        .then((data)=>{
+            const noteData = data.rows;
+            const filteredData = noteData.map((note) => {
+                return{
+                    noteId: note.noteid,
+                    heading: note.heading,
+                    content: note.content,
+                };
+            });
+
+            res.status(200).json({
+                message: "Success",
+                data: filteredData
+            });
+        })
+        .catch((err)=>{
+            res.status(400).json({
+                error: "Database Error Occured"
+            });
+        });
+};
+
+exports.updateNote = (req, res) => {
+    const noteId = req.noteId;
+    const { heading, content } = req.body;
+    client
+        .query(`UPDATE notes SET heading='${heading}', content = '${content}' WHERE noteid='${noteId}'`)
+        .then((data)=>{
+            res.status(200).json({
+                message: "Note Updated Successfully",
+            });
+        })
+        .catch((err)=>{
+            res.status(400).json({
+                error: "Database Error Occured"
+            });
+        });
+};
+
+exports.deleteNote = (req, res) => {
+    const noteId = req.noteId;
+    client
+        .query(`DELETE FROM notes WHERE noteid='${noteId}'`)
+        .then((data)=>{
+            res.status(200).json({
+                message: "Note Deleted Successfully",
+            });
+        })
+        .catch((err)=>{
+            res.status(400).json({
+                error: "Database Error Occured"
+            });
+        });
 };
--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const { verifyToken } = require("../middlewares/authMiddleware");
-const { addNote } = require("../controllers/notes");
+const { addNote, getAllNotes, updateNote, deleteNote } = require("../controllers/notes");
+const { handleNoteIdParam } = require("../middlewares/noteMiddleware");
 const router = express.Router();
 
+router.param("noteId", handleNoteIdParam);
+
 router.post("/add", verifyToken, addNote);
+router.get("/getallnotes", verifyToken, getAllNotes);
+router.put("/update/:noteId", verifyToken, updateNote);
+router.delete("/delete/:noteId", verifyToken, deleteNote);
 
 module.exports = router;
